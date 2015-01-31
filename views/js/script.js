2ee4fcd6413616a3a9b32f1def0cca31426a0102
franka92 <giulia.franchini2@studio.unibo.it>
2015-01-31 13:36:11 +0100

    Sistemato script.js, sistemate categorie

--- views/js/script.js
+++ views/js/script.js
@@ -12,13 +12,43 @@ $(document).ready(main);
 
 
 function main(){
-	visualizza_associazioni();
+	/*Visualizzo l'elenco completo della ssociazioni*/
+	get_associazioni();
+	/*Creo le checbox per i filtri delle categorie*/
 	crea_filtri_cat();
-	gestisci_query();
+	/*Creo le checbox per i filtri dei comuni*/
 	recupera_elenco_comuni();
+	/*Creo i grafici per la scheda "Statistiche"*/
+	crea_grafico_categorie();
+	crea_grafico_dati();
+	/*Gestione seleziona/deseleziona tutto*/
+	gestione_check_all();
+	/*Gestione bottoni "esegui query"*/
+	click_bt_query();
+	/*Tooltip*/
 	$("#help_query").popover();	
 	
+	/*Gestione click sul bottone per la "ricerca per nome"*/
+	$('#bt_ricerca').click(function(){
+		var input = $("#text_ricerca").val();
+		cerca_associazione(input);
 	
+	});
+	
+	/*Gestione pulsante "Scroll to top"*/
+	$(document).on( 'scroll', function(){
+		if ($(window).scrollTop() > 100) {
+			$('.scroll-top-wrapper').addClass('show');
+		} else {
+			$('.scroll-top-wrapper').removeClass('show');
+		}
+	});
+	$('.scroll-top-wrapper').on('click', scrollToTop);
+	
+}
+
+function gestione_check_all(){
+	/*Gestione checkbox "seleziona/deseleziona tutto relativamente ai filtri globali*/
 	$("#check_sel_tutto").change(function() {
         if($(this).is(":checked")) {
             $("input[name='check_query']").each(function(){
@@ -39,40 +69,53 @@ function main(){
         }
     });
 	
-	$('#bt_ricerca').click(function(){
-		var input = $("#text_ricerca").val();
-		cerca_associazione(input);
+	/*Gestione checkbox "seleziona/deseleziona tutto relativamente ai filtri di categoria*/
+	$("#cat_sel_tutto").change(function() {
+        if($(this).is(":checked")) {
+            $("input[name='check_cat']").each(function(){
+				$(this).prop('checked', 'true');
+			});
+			if($("#cat_des_tutto").is(":checked"))
+				$("#cat_des_tutto").trigger('click');
+        }
+    });
 	
-	});
+	$("#cat_des_tutto").change(function() {
+        if($(this).is(":checked")) {
+            $("input[name='check_cat']").each(function(){
+				$(this).removeAttr('checked');
+			});
+			if($("#cat_sel_tutto").is(":checked"))
+				$("#cat_sel_tutto").trigger('click');
+        }
+    });
 	
-	$("#bt_query_comuni").click(function(){
-		/*Encoding della query in modo da poterla inviare correttamente*/
-		var query = crea_query_comuni();
-		if(query != 0){
-			var encodedquery = encodeURIComponent(prefissi+query);
-			/*Dichiaro il formato dell'output*/
-			var url = endpointURL + "query?query=" + encodedquery + "&format=" + "json";
-				$.ajax({
-					method: 'GET',
-					url: url,
-					success: function (d){
-						visualizza_elenco(d.results.bindings);
-						
-					},
-					error: function (jqXHR, textStatus, errorThrown){
-						alert('Errore nel caricamento della lista dei documenti'+errorThrown);
-					}
-				 
-				});
-			visualizza_query(query);
-		}
-		else{
-			alert("Devi selezionare una o più categorie");
-		}
-	});
+	/*Gestione checkbox "seleziona/deseleziona tutto relativamente ai filtri sui comuni*/
+	$("#com_sel_tutto").change(function() {
+        if($(this).is(":checked")) {
+            $("input[name='check_com']").each(function(){
+				$(this).prop('checked', 'true');
+			});
+			if($("#com_des_tutto").is(":checked"))
+				$("#com_des_tutto").trigger('click');
+        }
+    });
 	
+	$("#com_des_tutto").change(function() {
+        if($(this).is(":checked")) {
+            $("input[name='check_com']").each(function(){
+				$(this).removeAttr('checked');
+			});
+			if($("#com_sel_tutto").is(":checked"))
+				$("#com_sel_tutto").trigger('click');
+        }
+    });
+
 }
 
+/*Ricerca di un'associazione in base all'input inserito dall'utente
+	@param filtro: input su cui filtrare la ricerca
+*/
 function cerca_associazione(filtro){
 	var query = "SELECT ?nome_associazione ?link ?stato ?regione ?cap ?locality ?indirizzo\
 				(GROUP_CONCAT(DISTINCT ?email ; separator= ' ; ') AS ?c_email)\
@@ -116,21 +159,24 @@ function cerca_associazione(filtro){
 			method: 'GET',
 			url: queryUrl,
 			success: function (d){
+				/*Visualizzo l'elenco dei risultati*/
 				visualizza_elenco(d.results.bindings);
 				$("#tab_risultati").find('span.filtri').remove();
 				$("#tab_risultati").prepend("<span class='filtri'>Hai cercato: <span class='filtro_ricerca'>"+filtro+"</span> - </span>");
+				/*Visualizzo la query effettuata*/
 				visualizza_query(query);
 				
 			},
 			error: function (jqXHR, textStatus, errorThrown){
-				alert('Errore nel caricamento della lista dei documenti'+errorThrown);
+				alert('Errore nella ricerca dell\'associazione'+textStatus);
+				console.log(errorThrown);
 			}
 		 
 		});
 }
 
 /*Funzione che recupera l'elenco totale delle associazioni*/
-function visualizza_associazioni(){
+function get_associazioni(){
 	var query = "SELECT ?nome_associazione ?link ?stato ?regione ?cap ?locality ?indirizzo\
 				(GROUP_CONCAT(DISTINCT ?email ; separator= ' ; ') AS ?c_email)\
 				(GROUP_CONCAT(DISTINCT ?numero ; separator= ' ; ') AS ?c_numeri)\
@@ -171,18 +217,23 @@ function visualizza_associazioni(){
 		method: 'GET',
 		url: queryUrl,
 		success: function (d){
+			/*Visualizzo l'elenco dei risultati e la query effettuata*/
 			visualizza_elenco(d.results.bindings);
 			visualizza_query(query);
 			
 		},
 		error: function (jqXHR, textStatus, errorThrown){
-			alert('Errore nel caricamento della lista dei documenti'+errorThrown);
+			alert('Errore nel caricamento dei dati'+textStatus);
+			console.log(errorThrown);
 		}
 	 
 	});
 
 }
 
+/*Visualizza la query che è stata effettuata
+	@param query: testo con la query effettuata
+*/
 function visualizza_query(query){
 
 	$prefix = prefissi.replace(/</g,'&lt;');
@@ -190,6 +241,7 @@ function visualizza_query(query){
 
 	$("#div_query").find("#query_sparql").remove();
 	$("#div_query").append(div_query);
+	/*Elimino i tab in modo da rendere la query più leggibile*/
 	$('pre').html(function() {
 		return this.innerHTML.replace(/\t{1,}/g, '<br>');
 	});
@@ -260,11 +312,9 @@ function visualizza_elenco(data){
 		$("#tot_res").html("0");
 	}
 	
-	
-
 }
 
-/*Crea i radio button per la selezione delle categorie*/
+/*Crea le checjbox per la selezione delle categorie*/
 function crea_filtri_cat(){
 
 	var query = "SELECT ?categoria ?descrizione WHERE{?categoria a skos:Concept;rdfs:label ?descrizione.} ORDER BY ?descrizione";
@@ -277,7 +327,6 @@ function crea_filtri_cat(){
 		method: 'GET',
 		url: queryUrl,
 		success: function (d){
-			
 			$dati = d.results.bindings;
 			for(var i=0;i<$dati.length;i++){
 				$value =$dati[i].descrizione.value;
@@ -288,13 +337,10 @@ function crea_filtri_cat(){
 					$("#filtri_cat").append("<p>"+$check+"</p>");
 				}
 			}
-			evento_click_bt();
-			crea_grafico_categorie();
-			crea_grafico_dati();
-			
 		},
 		error: function (jqXHR, textStatus, errorThrown){
-			alert('Errore nel caricamento della lista dei documenti'+errorThrown);
+			alert('Errore nel caricamento delle categorie'+textStatus);
+			console.log(errorThrown);
 		}
 	 
 	});
@@ -304,12 +350,36 @@ function crea_filtri_cat(){
 	
 	
 }
-/*Associa l'evento on-click sul pulsante di esecuzione della query (per categorie)*/
-function evento_click_bt(){
+/*Associa l'evento on-click sul pulsante di esecuzione della query*/
+function click_bt_query(){
 
-	$("#bt_query_cat").click(function(){
+	/*Gestione click sul bottone "esegui query" nei filtri generali*/
+	$("#bt_query").click(function(){
 		/*Encoding della query in modo da poterla inviare correttamente*/
-		var query = crea_query_cat()
+		var query = crea_query()
+		var encodedquery = encodeURIComponent(prefissi+query);
+		/*Dichiaro il formato dell'output*/
+		var url = endpointURL + "query?query=" + encodedquery + "&format=" + "json";
+			$.ajax({
+				method: 'GET',
+				url: url,
+				success: function (d){
+					visualizza_elenco(d.results.bindings);
+					visualizza_query(query);
+					
+				},
+				error: function (jqXHR, textStatus, errorThrown){
+					alert('Errore nel caricamento dei dati'+textStatus);
+					console.log(errorThrown);
+				}
+			 
+			});
+	});
+
+	/*Gestione click sul bottone "esegui query" in filtra per comune*/
+	$("#bt_query_comuni").click(function(){
+		/*Encoding della query in modo da poterla inviare correttamente*/
+		var query = crea_query_comuni();
 		if(query != 0){
 			var encodedquery = encodeURIComponent(prefissi+query);
 			/*Dichiaro il formato dell'output*/
@@ -333,37 +403,39 @@ function evento_click_bt(){
 		}
 	});
 
-
-}
-
-/*Associa l'evento on-click sul pulsante di esecuzione della query (per campi obbligatori)*/
-function gestisci_query(){
-	
-	$("#bt_query").click(function(){
-		/*Encoding della query in modo da poterla inviare correttamente*/
-		var query = crea_query()
-		var encodedquery = encodeURIComponent(prefissi+query);
-		/*Dichiaro il formato dell'output*/
-		var url = endpointURL + "query?query=" + encodedquery + "&format=" + "json";
-			$.ajax({
-				method: 'GET',
-				url: url,
-				success: function (d){
-					visualizza_elenco(d.results.bindings);
-					
-				},
-				error: function (jqXHR, textStatus, errorThrown){
-					alert('Errore nel caricamento della lista dei documenti'+errorThrown);
-				}
-			 
-			});
+	/*Gestione click sul bottone "esegui query" in filtra per categoria*/
+	$("#bt_query_cat").click(function(){	
+		var query = crea_query_cat()
+		if(query != 0){
+			/*Encoding della query in modo da poterla inviare correttamente*/
+			var encodedquery = encodeURIComponent(prefissi+query);
+			/*Dichiaro il formato dell'output*/
+			var url = endpointURL + "query?query=" + encodedquery + "&format=" + "json";
+				$.ajax({
+					method: 'GET',
+					url: url,
+					success: function (d){
+						visualizza_elenco(d.results.bindings);
+					},
+					error: function (jqXHR, textStatus, errorThrown){
+						alert('Errore nel caricamento dei dati'+textStatus);
+						console.log(errorThrown);
+					}
+				 
+				});
 			visualizza_query(query);
+		}
+		else{
+			alert("Devi selezionare una o più categorie");
+		}
 	});
-	
+
 
 }
 
-/*Crea dinamicamente la query in basei alle selezioni dell'utente*/
+/*Crea dinamicamente la query in base alle selezioni dell'utente
+	@return: la query creata
+*/
 function crea_query(){
 	$filtri = [];
 	$check_sito = $("#check_sito").prop('checked');
@@ -456,11 +528,13 @@ function crea_query(){
 
 }
 
-/*Crea dinamicamente la query in basei alle selezioni dell'utente*/
+/*Crea dinamicamente la query in base alle selezioni dell'utente (relativamente alle categorie)
+	@return: la query creata o 0 altrimenti
+*/
 
 function crea_query_cat(){
 	var filtro = "";
-
+	/*Recupero i valori delle checkbox selezionate*/
 	 $("input[name='check_cat']").each(function(){
 		var checked = $(this).prop('checked');
 		if(checked == true){
@@ -470,6 +544,7 @@ function crea_query_cat(){
 				filtro +=" || ?purp_label = '"+$(this).val()+"'";
 		}
 	});
+	/*Se è stato selezionato qualcosa*/
 	if(filtro != ""){
 		filtro += ")";
 		$value = $("input[name='radio_cat']:checked").val();
@@ -513,10 +588,12 @@ function crea_query_cat(){
 
 }
 
-
+/*Crea dinamicamente la query in base alle selezioni dell'utente (relativamente ai comuni)
+	@return: la query creata o 0 altrimenti
+*/
 function crea_query_comuni(){
 	var filtro = "";
-
+	/*Recupero i valori delle checkbox selezionate*/
 	 $("input[name='check_com']").each(function(){
 		var checked = $(this).prop('checked');
 		if(checked == true){
@@ -526,6 +603,7 @@ function crea_query_comuni(){
 				filtro +=" || ?locality = '"+$(this).val()+"'";
 		}
 	});
+	/*Se è stato selezionato qualcosa*/
 	if(filtro != ""){
 		filtro += ")";
 		$value = $("input[name='radio_cat']:checked").val();
@@ -567,7 +645,7 @@ function crea_query_comuni(){
 
 }
 
-
+/*Crea il grafico relativo alla percentuale per categoria*/
 function crea_grafico_categorie(){
 	var count = [];
 
@@ -585,11 +663,12 @@ function crea_grafico_categorie(){
 				url: url,
 				success: function (d){
 					$data = d.results.bindings;
+					/*Creo l'array con l'associazione "categoria-numero di risultati"*/
 					for(var i=0;i<$data.length;i++){
 						if($data[i].label.value != "Associazione no profit")
 							count.push({y: Number($data[i].count.value), indexLabel: $data[i].label.value});
 					}	
-					
+					/*Creo e visualizzo il grafico*/
 					var chart = new CanvasJS.Chart("chartCategorie",{
 					theme: "theme1",
 							title :{
@@ -609,7 +688,8 @@ function crea_grafico_categorie(){
 									
 				},
 				error: function (jqXHR, textStatus, errorThrown){
-					alert('Errore nel caricamento della lista dei documenti'+errorThrown);
+					alert('Errore nel recupero dei dati'+textStatus);
+					console.log(errorThrown);
 				}
 			 
 			});
@@ -618,6 +698,7 @@ function crea_grafico_categorie(){
 
 }
 
+/*Crea il grafico relativo alla percentuale di dati raccolti*/
 function crea_grafico_dati(){
 	var tot_siti = count_siti();
 	var tot_luogo = count_luogo();
@@ -645,6 +726,9 @@ function crea_grafico_dati(){
 
 }
 
+/*Recupera il numero totale di associazioni a cui è associato in sito web
+	@return: numero di risultati recuperati
+*/
 function count_siti(){
 	var query='SELECT (count (DISTINCT ?org) as ?count) WHERE{\
 					?org a org:Organization;\
@@ -663,13 +747,18 @@ function count_siti(){
 								
 			},
 			error: function (jqXHR, textStatus, errorThrown){
-				alert('Errore nel caricamento della lista dei documenti'+errorThrown);
+				alert('Errore nel recupero dei dati'+textStatus);
+				console.log(errorThrown);
 			}
 		 
 		});
 		
 		return count;
 }
+
+/*Recupera il numero totale di associazioni a cui sono associate informazioni sul luogo
+	@return: numero di risultati recuperati
+*/
 function count_luogo(){
 	var query="SELECT (count (DISTINCT ?org) as ?count) WHERE{\
 				?org a org:Organization;\
@@ -691,13 +780,17 @@ function count_luogo(){
 								
 			},
 			error: function (jqXHR, textStatus, errorThrown){
-				alert('Errore nel caricamento della lista dei documenti'+errorThrown);
+				alert('Errore nel recupero dei dati'+textStatus);
+				console.log(errorThrown);
 			}
 		 
 		});
 		return count;
 }
 
+/*Recupera il numero totale di associazioni a cui sono associati dei contatti email
+	@return: numero di risultati recuperati
+*/
 function count_email(){
 	var query="SELECT (count (DISTINCT ?org) as ?count) WHERE{\
 				?org a org:Organization;\
@@ -719,13 +812,17 @@ function count_email(){
 								
 			},
 			error: function (jqXHR, textStatus, errorThrown){
-				alert('Errore nel caricamento della lista dei documenti'+errorThrown);
+				alert('Errore nel recupero dei dati'+textStatus);
+				console.log(errorThrown);
 			}
 		 
 		});
 		return count;
 }
 
+/*Recupera il numero totale di associazioni a cui sono associati dei contatti telefonici
+	@return: numero di risultati recuperati
+*/
 function count_telefono(){
 	var query="SELECT (count (DISTINCT ?org) as ?count) WHERE{\
 				?org a org:Organization;\
@@ -747,13 +844,15 @@ function count_telefono(){
 								
 			},
 			error: function (jqXHR, textStatus, errorThrown){
-				alert('Errore nel caricamento della lista dei documenti'+errorThrown);
+				alert('Errore nel recupero dei dati'+textStatus);
+				console.log(errorThrown);
 			}
 		 
 		});
 		return count;
 }
 
+/*Recupera l'elenco dei comuni relativamente alla provincia di Bologna*/
 function recupera_elenco_comuni(){
 	$.ajax({
         type: "GET",
@@ -764,17 +863,21 @@ function recupera_elenco_comuni(){
 
 }
 
-function crea_check_comuni(allText) {
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(';');
+/*Crea le checkbox per la selezione dei comuni
+	@param data: oggetto che contiene i dati da recuperare
+*/
+function crea_check_comuni(data) {
+    var dataLines = data.split(/\r\n|\n/);
+	/*Headers del file .csv*/
+    var headers = dataLines[0].split(';');
     var comuni = [];
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(';');
+	/*Ciclo su ogni riga ( = riga del file csv da cui ho letto i dati), saltando la prima (headers) */
+    for (var i=1; i<dataLines.length; i++) {
+        var data = dataLines[i].split(';');
         if (data.length == headers.length) {
             for (var j=0; j<headers.length; j++) {
+				/*Se il campo "Provincia" corrisponde a Bologna (BO) --> creo la checkbox*/
 				if(headers[j] == "Provincia" && data[j] == "BO"){
-					console.log(data[j-1]);
-					//$c =$data[j-1]
 					$id="check-"+data[j-1].replace(" ","_");
 					$check = '<input type="checkbox" class="check_cat" id="'+$id+'" value="'+data[j-1].replace("'","\\'")+'" name="check_com"/><span>'+data[j-1]+'</span>';
 					$("#filtri_comune").append("<p>"+$check+"</p>");
@@ -782,11 +885,17 @@ function crea_check_comuni(allText) {
 
             }
         }
-    }	
-	
+    }		
+}
 
-	
-	
+
+/*Riporta il focus sul top della pagina*/
+function scrollToTop() {
+	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+	element = $('body');
+	offset = element.offset();
+	offsetTop = offset.top;
+	$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
 }
 
 	
